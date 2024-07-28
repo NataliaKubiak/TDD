fd8ccff64ec43035f30946f673a189d72770755a
Natalia Kubiak <nataliia.kubiak@gmail.com>
2024-07-28 18:19:34 +0200

    refactor PhoneBookTest

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -3,19 +3,22 @@ import org.junit.jupiter.api.Test;
 
 public class PhoneBookTest {
 
-    PhoneBook phoneBook = new PhoneBook();
-
     @Test
     public void test_addContact() {
+        PhoneBook phoneBook = new PhoneBook();
         int expectedContactAmount = 1;
-        int actualContactAmount = phoneBook.add("Natalia Ivanova", "9056708754");
+
+        int actualContactAmount = phoneBook.add("Ivan Popov", "9056708754");
 
         Assertions.assertEquals(expectedContactAmount, actualContactAmount);
     }
 
     @Test()
     public void test_addSameContact() {
+        PhoneBook phoneBook = new PhoneBook();
         int expectedContactAmount = 1;
+        phoneBook.add("Natalia Ivanova", "9056708754");
+
         int actualContactAmount = phoneBook.add("Natalia Ivanova", "9056708754");
 
         Assertions.assertEquals(expectedContactAmount, actualContactAmount);
@@ -23,8 +26,10 @@ public class PhoneBookTest {
 
     @Test
     public void test_positive_findByNumber() {
+        PhoneBook phoneBook = new PhoneBook();
         String expectedName = "Elena Petrova";
         phoneBook.add("Elena Petrova", "9112345678");
+
         String actualName = phoneBook.findByNumber("9112345678");
 
         Assertions.assertEquals(expectedName, actualName);
@@ -32,6 +37,8 @@ public class PhoneBookTest {
 
     @Test
     public void test_negative_findByNumber() {
+        PhoneBook phoneBook = new PhoneBook();
+
         String actualName = phoneBook.findByNumber("9111111111");
 
         Assertions.assertNull(actualName);
